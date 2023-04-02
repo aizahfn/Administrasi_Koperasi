9a4d08138f8b1b08f78412a6b1c2f1f9f9d38243
ahnaf <90494256+ahnafb@users.noreply.github.com>
2023-04-03 02:00:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
-  
+
 use Illuminate\Support\Facades\Route;
-  
+
+use App\Http\Controllers\KoperasiController;
+use App\Http\Controllers\LowonganController;
+use App\Http\Controllers\BerkasController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ArsipController;
 
 
-  
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,17 +19,11 @@ use App\Http\Controllers\ArsipController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-    
+
+Route::resource('koperasi', KoperasiController::class);
+Route::resource('datalowongan', LowonganController::class);
+Route::resource('berkas', BerkasController::class);
+Route::resource('user', UserController::class);
+
 Route::resource('arsip', ArsipController::class);
 Route::get('/arsips/create', [ArsipController::class, 'create'])->name('arsips.create');
-// Route::get('/arsip', 'ArsipController@index')->name('arsip.index');
-// Route::delete('/arsip/{id}', 'ArsipController@destroy')->name('arsip.destroy');
-// Route::get('/arsip', 'App\Http\Controllers\Admin\ArsipController@index');
-// Route::get('/arsip', [ArsipController::class, 'index']);
-// Route::put('/arsip/{arsip}', [ArsipController::class, 'update'])->name('arsip.update');
-// Route::post('/arsip', 'ArsipController@store')->name('arsip.store');
-// Route::get('/arsip', [ArsipController::class, 'index']);
-// Route::get('/arsips/{id}', [ArsipController::class, 'show'])->name('arsips.show');
-// Route::get('/arsips/{id}/edit', [ArsipController::class, 'edit'])->name('arsips.edit');
-// Route::get('/arsip', [ArsipController::class, 'index'])->name('arsip.index');
-// Route::get('/arsips', [ArsipController::class, 'index']);
