5b193004e60a07c5118fe0725a22f0e5469a460d
RizkiWijanarko2 <rizkiwijanarko4@gmail.com>
2023-05-07 05:50:29 +0700

    Administrasi_Koprasi

---
+++ app/Http/Controllers/LowonganController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lowongan;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class LowonganController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return response()
+     */
+    public function index(): View
+    {
+        $datalowongan = Lowongan::latest()->paginate(5);
+        return view('datalowongan.index',compact('datalowongan'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('datalowongan.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'nama_lowongan' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'tanggal_lowongan' => 'required',
+            'jumlah_lowongan' => 'required',
+            'deskripsi_lowongan' => 'required'
+        ]);
+
+        $input = $request->all();
+
+        if ($image = $request->file('image')) {
+            $destinationPath = 'images/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }
+
+        Lowongan::create($input);
+
+        return redirect()->route('datalowongan.index')
+                        ->with('success','Lowongan created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lowongan $lowongan): View
+    {
+        return view('datalowongan.show',compact('lowongan'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lowongan $lowongan): View
+    {
+        return view('datalowongan.edit',compact('lowongan'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Lowongan $lowongan): RedirectResponse
+    {
+        $request->validate([
+            'nama_lowongan' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'tanggal_lowongan' => 'required',
+            'jumlah_lowongan' => 'required',
+            'deskripsi_lowongan' => 'required'
+        ]);
+
+        $input = $request->all();
+
+        if ($image = $request->file('image')) {
+            $destinationPath = 'images/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }else{
+            unset($input['image']);
+        }
+
+        $lowongan->update($input);
+
+        return redirect()->route('datalowongan.index')
+                        ->with('success','Lowongan updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lowongan $lowongan): RedirectResponse
+    {
+        $lowongan->delete();
+
+        return redirect()->route('datalowongan.index')
+                        ->with('success','Lowongan deleted successfully');
+    }
+}
---
+++ resources/views/arsip/index.blade.php
@@ -0,0 +1,66 @@
+@extends('arsip.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Arsip</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('arsips.create') }}"> Create New Arsip</a>
+            </div>
+        </div>
+    </div>
+    
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+     
+    <table class="table table-bordered">
+        <tr>
+            <th>No Dokumen</th>
+            <th>Nama Dokumen</th>
+            <th>Kategori Dokumen</th>
+            <th>Tanggal Dokumen</th>
+            <th>Isi Dokumen</th>
+            <th>Sumber Dokumen</th>
+            <th>Penerima Dokumen</th>
+            <th>Status Dokumen</th>
+            <th>Aksesibilitas Dokumen</th>
+            <th width="220px">Action</th>
+        </tr>
+        @foreach ($arsips as $arsip)
+        <tr>
+            <!-- <td>{{ ++$i }}</td> -->
+            <!-- <td><img src="/images/{{ $arsip->image }}" width="100px"></td> -->
+            <td>{{ $arsip->no_dokumen }}</td>
+            <td>{{ $arsip->nama_dokumen }}</td>
+            <td>{{ $arsip->kategori_dokumen }}</td>
+            <td>{{ $arsip->tanggal_dokumen }}</td>
+            <td>{{ $arsip->isi_dokumen }}</td>
+            <td>{{ $arsip->sumber_dokumen }}</td>
+            <td>{{ $arsip->penerima_dokumen }}</td>
+            <td>{{ $arsip->status_dokumen }}</td>
+            <td>{{ $arsip->aksesibilitas_dokumen }}</td>
+            <td>
+                <form action="{{ route('arsip.destroy',$arsip->id) }}" method="POST">
+     
+                    <a class="btn btn-info" href="{{ route('arsip.show',$arsip->id) }}">Show</a>
+      
+                    <a class="btn btn-primary" href="{{ route('arsip.edit',$arsip->id) }}">Edit</a>
+     
+                    @csrf
+                    @method('DELETE')
+        
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+    
+    <!-- {!! $arsips->links() !!} -->
+        
+@endsection
