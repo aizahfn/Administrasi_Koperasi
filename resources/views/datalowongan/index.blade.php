20164fb86acd9f7d5cb1bf36985dac14d708377f
rizkiwijanarko <rizkiwijanarko@students.unnes.ac.id>
2023-05-25 09:50:11 +0700

    fix

--- resources/views/datalowongan/index.blade.php
+++ resources/views/datalowongan/index.blade.php
@@ -1,58 +1,153 @@
-@extends('datalowongan.layout')
+<x-layout bodyClass="g-sidenav-show  bg-gray-200">
 
-@section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>CRUD Lowongan</h2>
+    <x-navbars.sidebar activePage="informasi-pendaftar"></x-navbars.sidebar>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <!-- Navbar -->
+        <x-navbars.navs.auth titlePage="User Management"></x-navbars.navs.auth>
+        <!-- End Navbar -->
+        <div class="container-fluid py-4">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card my-4">
+                        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                            <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                                <h3 class="text-white mx-3"><strong> MANAJEMEN LOWONGAN</strong></h3>
+                            </div>
+                        </div>
+                        <div class=" me-3 my-3 text-end">
+                            <a class="btn bg-gradient-dark mb-0" href="{{ route('berkas.create') }}"><i
+                                    class="material-icons text-sm">add</i>&nbsp;&nbsp;Tambahkan Lowongan</a>
+                        </div>
+                        <div class="card-body px-0 pb-2">
+                            <div class="table-responsive p-0">
+                                <table class="table align-items-center mb-0" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th
+                                                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                ID LOWONGAN
+                                            </th>
+                                            <th
+                                                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                GAMBAR</th>
+                                            <th
+                                                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                                LOWONGAN</th>
+                                            <th
+                                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                BATAS AKHIR</th>
+                                            <th
+                                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                JUMLAH YANG DIBUTUHKAN</th>
+                                            <th
+                                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                DESKRIPSI LOWONGAN</th>
+                                            <th class="text-secondary opacity-7"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($datalowongan as $lowongan)
+                                            <tr>
+                                                <td>
+                                                    <div class="d-flex px-2 py-1">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <p class="mb-0 text-sm">{{ $user->id }}</p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex px-2 py-1">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <p class="mb-0 text-sm">{{ $user->nama_lengkap }}</p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex px-2 py-1">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <p class="mb-0 text-sm">{{ $user->no_telp }}</p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex px-2 py-1">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <p class="mb-0 text-sm">{{ $user->nama_lowongan }}</p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex px-2 py-1">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <p class="mb-0 text-sm">{{ $user->email }}</p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex px-2 py-1">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <p class="mb-0 text-sm">{{ $user->password }}</p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex px-2 py-1">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <p class="mb-0 text-sm">{{ $user->alamat }}</p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex px-2 py-1">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <p class="mb-0 text-sm">{{ $user->jenis_kelamin }}</p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex px-2 py-1">
+                                                        <div class="d-flex flex-column justify-content-center">
+                                                            <p class="mb-0 text-sm">{{ $user->tanggal_lahir }}</p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="align-middle">
+                                                    <a rel="tooltip" class="btn btn-success btn-link"
+                                                        href="{{ route('user.edit', $user->id) }}" data-original-title=""
+                                                        title="">
+                                                        <i class="material-icons">edit</i>
+                                                        <div class="ripple-container"></div>
+                                                    </a>
+                                                    <form id="delete-form" action="{{ route('user.destroy', $user->id) }}" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-danger btn-link" data-original-title="" title="">
+                                                            <i class="material-icons">close</i>
+                                                            <div class="ripple-container"></div>
+                                                        </button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="11">
+                                                    <div class="alert alert-danger">
+                                                        Data Pendaftar belum Tersedia.
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                                {{ $users->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('datalowongan.create') }}"> Create New Lowongan</a>
-            </div>
+            <x-footers.auth></x-footers.auth>
         </div>
-    </div>
-    
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-     
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Gambar</th>
-            <th>Lowongan</th>
-            <th>Batas Akhir</th>
-            <th>Jumlah Yang Dibutuhkan</th>
-            <th>Deskripsi</th>
-            <th width="280px">Action</th>
-        </tr>
-        @foreach ($datalowongan as $lowongan)
-        <tr>
-            <td>{{ ++$i }}</td>
-            <td><img src="/images/{{ $lowongan->image }}" width="100px"></td>
-            <td>{{ $lowongan->nama_lowongan }}</td>
-            <td>{{ $lowongan->tanggal_lowongan }}</td>
-            <td>{{ $lowongan->jumlah_lowongan }}</td>
-            <td>{{ $lowongan->deskripsi_lowongan }}</td>
-            <td>
-                <form action="{{ route('datalowongan.destroy',$lowongan->id) }}" method="POST">
-     
-                    <a class="btn btn-info" href="{{ route('datalowongan.show',$lowongan->id) }}">Show</a>
-      
-                    <a class="btn btn-primary" href="{{ route('datalowongan.edit',$lowongan->id) }}">Edit</a>
-     
-                    @csrf
-                    @method('DELETE')
-        
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-    
-    {!! $datalowongan->links() !!}
-        
-@endsection
+    </main>
+    <x-plugins></x-plugins>
+
+</x-layout>
