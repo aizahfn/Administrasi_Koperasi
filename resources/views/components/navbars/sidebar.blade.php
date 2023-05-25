f6b06b922021d5ff763a45c00ffdd7ba25aa9127
ahnaf <90494256+ahnafb@users.noreply.github.com>
2023-05-25 14:13:43 +0700

    Update sidebar.blade.php

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -49,7 +49,7 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white {{ $activePage == 'surat-masuk' ? ' active bg-gradient-primary' : '' }} "
-                    href="{{ route('surat-masuk') }}">
+                    href="{{ route('arsip.index') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">dashboard</i>
                     </div>
